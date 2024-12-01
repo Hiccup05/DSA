38319659de90574e34b2d4d01bd8b9ebd39f3a40
Hiccup05 <paudelbipin05@gmail.com>
2024-12-01 18:41:02 +0545

    Detail of CSLL

---
+++ CSLL.java
@@ -0,0 +1,14 @@
+public class CSLL {
+    node last;
+    private class node {
+        int data;
+        node next;
+        public node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
