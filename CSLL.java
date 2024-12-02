421a397045f0a0190ac242300b683a94e0b6a4be
Bipin Paudel <paudelbipin05@gmail.com>
2024-12-02 08:17:38 +0545

    basic circular list function

--- CSLL.java
+++ CSLL.java
@@ -1,5 +1,6 @@
 public class CSLL {
     node last;
+    int length;
     private class node {
         int data;
         node next;
@@ -8,7 +9,54 @@ public class CSLL {
             this.next = null;
         }
     }
-    public static void main(String[] args) {
+    public CSLL(){
+        last=null;
+        length=0;
+    }
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length==0;
+    }
+    public void simpleCSLL(){
+        node first=new node(1);
+        node second=new node(2);
+        node third=new node(3);
+        node fourth=new node(4);
 
+        first.next=second;
+        second.next=third;
+        third.next=fourth;
+        fourth.next=first;
+
+        last=fourth; //as we track last node need to refrence fourth node as last as its a last node
+        System.out.println("I have come here");
+    }
+    public void insert(int data) {
+        node newNode = new node(data);
+        node first;
+        if (last == null) {
+            last=newNode;
+            last.next=last;
+        }
+    }
+
+    public void print(){      if(last==null){
+            System.out.println("List is empty");
+            return;
+        }
+        node first=last.next;
+        while(first!=last){
+            System.out.print(first.data+"->");
+            first=first.next;
+        }
+        System.out.print(first.data);
+
+    }
+    public static void main(String[] args) {
+        CSLL csll=new CSLL();
+        csll.simpleCSLL();
+        csll.print();
     }
 }
